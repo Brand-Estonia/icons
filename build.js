b4786bf8d55c97836a6d78cc6e3bc0792f933011
sven-ra <mail@svenraju.net>
2024-11-27 11:58:19 +0200

    add type declaration

--- build.js
+++ build.js
@@ -23,6 +23,27 @@ async function build() {
   // Create index.js with all exports
   await fs.writeFile('index.js', exports.join('\n'));
 
+  // Create TypeScript declaration file
+  const typeDefinitions = [
+    '/// <reference types="react" />',
+    'import { SVGProps } from "react";',
+    '',
+    'declare module "@brand-estonia/icons" {',
+    ...files.map(file => {
+      const name = path
+      .basename(file, '.svg')
+      .replace(/[^a-zA-Z0-9]/g, ' ') // Replace non-alphanumeric chars with space
+      .replace(/\s+(\w)/g, (_, c) => c.toUpperCase()) // Capitalize first letter after space
+      .replace(/\s/g, ''); // Remove spaces
+      const capitalizedName = name.charAt(0).toUpperCase() + name.slice(1);
+      return `  export const ${capitalizedName}: (props: SVGProps<SVGSVGElement>) => JSX.Element;`
+    }),
+    '}',
+    ''
+  ].join('\n');
+
+  await fs.writeFile('index.d.ts', typeDefinitions);
+
   console.log(`Generated ${exports.length} icon exports`);
 }
 

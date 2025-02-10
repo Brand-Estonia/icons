67a9e0137e29b4ead84ffe371093aa979e8145a7
sven-ra <mail@svenraju.net>
2025-02-10 11:36:42 +0200

    update README with new icons and increment package version to 1.0.10

--- build.js
+++ build.js
@@ -104,6 +104,13 @@ async function build() {
 
   await fs.appendFile('README.md', readmeContent);
 
+  // update packages.json version number
+  const packageJson = require('./package.json');
+  const version = packageJson.version.split('.');
+  version[2] = parseInt(version[2]) + 1;
+  packageJson.version = version.join('.');
+  await fs.writeFile('package.json', JSON.stringify(packageJson, null, 2));
+
   console.log(`Generated ${components.length} icon components with type definitions`);
 }
 
